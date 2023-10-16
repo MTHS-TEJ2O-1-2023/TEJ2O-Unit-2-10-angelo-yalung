39748acfb8c23e8dcf3da19654c583f75acec577
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-16 13:26:57 -0400

    2

--- main.ts
+++ main.ts
@@ -5,12 +5,12 @@
  * This program lights up the neopixels when a certain light threshold is met
 */
 
-// variables
+  // variables
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-let lightLevel: number = input.lightLevel()
+let lightLevel: number
 let neopixelStrip: neopixel.Strip = null
 
-// cleanup
+  // cleanup
 basic.clearScreen()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
@@ -18,8 +18,15 @@ neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
 
+  // program starting on button a pressed
 input.onButtonPressed(Button.A, function() {
+
+  // looking for lightlevel
+  lightLevel = input.lightLevel()
+
+  // if the light level is less than or equal to 51
 if (lightLevel <= 51) {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -28,6 +35,7 @@ if (lightLevel <= 51) {
   neopixelStrip.show()
 }
 
+  // if the light level is less than or equal to 52
 if (lightLevel >= 52) {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -37,6 +45,7 @@ if (lightLevel >= 52) {
   basic.showNumber(lightLevel)
 }
 
+  // if the light level is less than or equal to 104
 if (lightLevel >= 104) {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
@@ -46,6 +55,7 @@ if (lightLevel >= 104) {
   basic.showNumber(lightLevel)
 }
 
+  // if the light level is greater than or equal to 156
 if (lightLevel >= 156) {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
@@ -55,6 +65,7 @@ if (lightLevel >= 156) {
   basic.showNumber(lightLevel)
 }
 
+  // if the light level is greater than or equal to 208
 if (lightLevel >= 208) {
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
@@ -65,6 +76,7 @@ if (lightLevel >= 208) {
 }
 })
 
+  // resetting the program
 input.onButtonPressed(Button.B, function () {
   basic.clearScreen()
   neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
@@ -73,4 +85,5 @@ input.onButtonPressed(Button.B, function () {
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
   neopixelStrip.show()
+  basic.showIcon(IconNames.Happy)
 })
