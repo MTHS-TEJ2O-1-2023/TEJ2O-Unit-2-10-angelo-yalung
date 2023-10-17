58e575f22002f964f3c4552f1ee77f889498a534
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-17 12:50:59 -0400

    shoud be done

--- main.ts
+++ main.ts
@@ -21,55 +21,55 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-// check if <= 51 light level
-    lightvalue = input.lightLevel()
-if (lightvalue <= 51) {
-  basic.showNumber(0)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  // check if <= 51 light level
+  lightvalue = input.lightLevel()
+  if (lightvalue <= 51) {
+    basic.showNumber(1)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     }
 
   // check if >= 52 light level
-if (lightvalue >= 52) {
-  basic.showNumber(0)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
+  if (lightvalue >= 52) {
+    basic.showNumber(0)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
 }
 
   // equal to or more than 104 light
-if (lightvalue >= 104) {
-  basic.showNumber(1)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
+  if (lightvalue >= 104) {
+    basic.showNumber(2)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
 }
 
   // 156 or more light
-if (lightvalue >= 156) {
-  basic.showNumber(2)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
+  if (lightvalue >= 156) {
+    basic.showNumber(3)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
 }
 
   // if the light value is greater than or equal to
-if (lightvalue >= 208) {
-  basic.showNumber(3)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-  neopixelStrip.show()
-    }
+  if (lightvalue >= 208) {
+    basic.showNumber(4)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+}
 })
 
-// 
+  // to reset the microbit 
 input.onButtonPressed(Button.B, function () {
   basic.clearScreen()
   neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
