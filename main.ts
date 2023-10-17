7798b0a732ce08d898f1f4b5b659546bac6eabcc
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-17 12:58:40 -0400

    iuo[

--- main.ts
+++ main.ts
@@ -26,14 +26,14 @@ input.onButtonPressed(Button.A, function () {
   if (lightvalue <= 51) {
     basic.showNumber(1)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    }
+  }
 
   // check if >= 52 light level
   if (lightvalue >= 52) {
     basic.showNumber(0)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
-}
+  }
 
   // equal to or more than 104 light
   if (lightvalue >= 104) {
@@ -42,7 +42,7 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
-}
+  }
 
   // 156 or more light
   if (lightvalue >= 156) {
@@ -53,7 +53,7 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
-}
+  }
 
   // if the light value is greater than or equal to
   if (lightvalue >= 208) {
@@ -66,10 +66,10 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
-}
+  }
 })
 
-  // to reset the microbit 
+// to reset the microbit
 input.onButtonPressed(Button.B, function () {
   basic.clearScreen()
   neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
